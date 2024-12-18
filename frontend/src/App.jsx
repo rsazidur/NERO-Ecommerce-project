133a98da6697d51a7ef8ffde0654f4ce98156234
rsazidur <rsazidur936@gmail.com>
2024-12-18 22:56:31 +0600

    Working with components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello Vite + React!</h1>
+      <Outlet />
     </>
   )
 }
