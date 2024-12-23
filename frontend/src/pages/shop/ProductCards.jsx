39bde84767342e89c464b876a33f7b1cac773d6e
rsazidur <rsazidur936@gmail.com>
2024-12-23 22:53:19 +0600

    Products Added

--- frontend/src/pages/shop/ProductCards.jsx
+++ frontend/src/pages/shop/ProductCards.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import RatingStars from '../../components/RatingStars'
 
 const ProductCards = ({products}) => {
     console.log(products)
@@ -8,12 +9,27 @@ const ProductCards = ({products}) => {
     {
         products.map((product, index) => (
             <div key={index} className='product__card'>
-                <div>
+                <div className='relative'>
                     <Link to={`/shop/${product._id}`}>
                     <img src={product.image} alt="product image" className='max-h-96 md:h-64
                     w-full object-cover hover:scale-105 transition-all duration-300' />
                     </Link>
+                    <div className='hover:block absolute top-3 right-3'>
+                        <button>
+                        <i className="ri-shopping-cart-2-line bg-primary p-1.5 text-white
+                        hover:bg-primary-dark"></i>
+                        </button>
+                    </div>
                 </div>
+
+                {/* Product Info */}
+                <div className='product__card__content'>
+                    <h4>{product.name}</h4>
+                    <p>${product.price} {product?.oldPrice ? <s>${product?.oldPrice}</s> : 
+                    null}</p>
+                    <RatingStars rating={product.rating} />
+                </div>
+
             </div>
         ))
 
--- frontend/src/pages/shop/TrendingProducts.jsx
+++ frontend/src/pages/shop/TrendingProducts.jsx
@@ -11,11 +11,22 @@ const TrendingProducts = () => {
   return (
     <section className='section__container product__container'>
         <h2 className='section__header'>Trending Products</h2>
-        <p className='section__subheader'> Discover the Hottest Picks: Find your
+        <p className='section__subheader m-12'> Discover the Hottest Picks: Find your
           favourite game at Best price</p>
     
         {/* Product Cards */}
-        <ProductCards products={products}/>
+        <div className='mt-12'>
+        <ProductCards products={products.slice(0, visibleProducts)}/>
+        </div>
+        
+        {/* Load More Button */}
+        <div className='product__btn'>
+            {
+                visibleProducts < products.length && (
+                    <button className='btn' onClick={loadMoreProducts}>Load More</button>
+                )
+            }
+        </div>
     </section>
   )
 }
