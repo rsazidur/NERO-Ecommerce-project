da16e0ed152d6e72e5624875e51b78179f6f0bfc
rsazidur <rsazidur936@gmail.com>
2025-01-01 21:32:07 +0600

    Category page in category section

---
+++ frontend/src/pages/category/CategoryPage.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+import product from '../../data/products.json'
+
+const CategoryPage = () => {
+    const {categoryName} = useParams();
+    const {filteredProducts, setFilteredProducts} = useProducts();
+
+    useEffect(() => {
+        const filtered = product.filter((product) => product.category === categoryName.toLowerCase
+        ());
+
+        setFilteredProducts(filtered);
+    }, [categoryName])
+  return (
+    <>
+    <section className='section__container bg-primary-light'>
+        <h2 className='section__header capitalize'>{categoryName}</h2>
+        <p className='section__subheader'> Browse a diverse range of categories, from Pc games to PS4 exclusives and more games
+        </p>
+    </section>
+    </>
+  )
+}
+
+export default CategoryPage
