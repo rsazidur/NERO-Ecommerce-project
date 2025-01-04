90d8673299d99fcabfaaf433d445894b5dfecdd2
rsazidur <rsazidur936@gmail.com>
2025-01-05 01:46:09 +0600

    Working With OrderSummary.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import CartModel from '../pages/shop/CartModel'
 
 const Navbar = () => {
 
     const products = useSelector((state) => state.cart.products);
+    const [isCartOpen, setIsCartOpen] = useState(false);
+    const handleCartToggle = () => {
+        setIsCartOpen(!isCartOpen);
+    }
 
   return (
     <header className='fixed-nav-bar w-nav'>
@@ -29,7 +35,7 @@ const Navbar = () => {
                     </Link>
                 </span>
                 <span>
-                    <button className='hover:text-primary'>
+                    <button onClick={handleCartToggle} className='hover:text-primary'>
                         <i class="ri-shopping-bag-line"></i>
                         <sup className='text-sm inline-block px-1.5 text-white rounded-full
                         bg-primary text-center'>{products.length}</sup>
@@ -42,6 +48,11 @@ const Navbar = () => {
                 </span>  
             </div>
         </nav>
+
+        {
+            isCartOpen && <CartModel products={products} isOpen={isCartOpen} onClose=
+            {handleCartToggle}/>
+        }
     </header>
   )
 }
