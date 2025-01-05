5d6e521473aec6c64e458d5e1e1aac6119d5fd13
rsazidur <rsazidur936@gmail.com>
2025-01-06 02:14:54 +0600

    MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const cors = require('cors')
 const app = express()
-const port = 3000
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// Middleware setup
+app.use(express.json({limit: '25mb'}));
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+console.log();
+
+main().then(()=> console.log("mongodb is successfully connected.")).catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(process.env.DB_URL);
+  
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World2!')
 })
 
 app.listen(port, () => {
