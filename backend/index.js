568c40074ac45953d2e9a2a745da5779b063e2fa
rsazidur <rsazidur936@gmail.com>
2025-01-06 04:02:17 +0600

    Working in mysql

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,17 @@ const mongoose = require('mongoose');
 const cors = require('cors')
 const app = express()
 require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
 // Middleware setup
 app.use(express.json({limit: '25mb'}));
+// app.use(express.urlencoded({limit: '25mb'}));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
